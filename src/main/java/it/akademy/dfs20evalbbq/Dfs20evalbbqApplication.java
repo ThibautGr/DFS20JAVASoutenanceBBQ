1c7f2cc3929a29b1c4d71632fefb06284d7a82d8
thibaut <thibaut.gerin@etu.univ-st-etienne.fr>
2021-04-08 15:24:12 +0200

    init commit

---
+++ src/main/java/it/akademy/dfs20evalbbq/Dfs20evalbbqApplication.java
@@ -0,0 +1,13 @@
+package it.akademy.dfs20evalbbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfs20evalbbqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dfs20evalbbqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/akademy/dfs20evalbbq/Dfs20evalbbqApplicationTests.java
@@ -0,0 +1,13 @@
+package it.akademy.dfs20evalbbq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dfs20evalbbqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
